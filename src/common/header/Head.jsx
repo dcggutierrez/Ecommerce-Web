e8b991aa54599667095dfc9722c25fc93792f17e
Dustine Gutierrez <dustine.gutierrez@whitecloak.com>
2023-03-03 23:23:43 +0800

    Initial Commit

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+        <section className="head">
+            <div className="container d_flex">
+                <div className="left row">
+                    <i className="fa fa-phone"></i>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Head
