4284760d3ada7c743410c94c35ce51dd673c3040
Dustine Gutierrez <dustine.gutierrez@whitecloak.com>
2023-03-05 00:47:56 +0800

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Header from './common/header/Header';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      generate a new App
+      <Router>
+        <Header/>
+        <Switch>
+          {/* <Route path='/about'>
+            <About/>
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -7,6 +7,17 @@ const Head = () => {
             <div className="container d_flex">
                 <div className="left row">
                     <i className="fa fa-phone"></i>
+                    <label>+63 916 435 8418</label>
+                    <i className="fa fa-envelope"></i>
+                    <label>dustgutierrez@gmail.com</label>
+                </div>
+                <div className="right row RText">
+                  <label>Theme FAQ's</label>
+                  <label>Need Helps</label>
+                  <span role="img" aria-label='none'>🇵🇭</span>
+                  <label >PH</label>
+                  <span role="img" aria-label='none'>🇺🇸</span>
+                  <label >US</label>
                 </div>
             </div>
         </section>
