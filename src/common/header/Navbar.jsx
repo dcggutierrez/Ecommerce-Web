0d84677448dd2fca8a5378a824204a8364854e6a
Dustine Gutierrez <dustine.gutierrez@whitecloak.com>
2023-03-05 02:25:28 +0800

    Start search component

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                 </h4>
             </div>
             <div className="navLink">
-              <ul className="nav">
+              <ul className={MobileMenu ? "nav-links-MobileMenu":"link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
                 <li>
                   <Link to='/'>home</Link>
                 </li>
--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import logo from '../../components/assets/images/logo.svg'
+import { Link } from 'react-router-dom'
 
 const Search = () => {
   return (
     <div>
-        <h1>Search</h1>
+        <section className="search">
+          <div className="container c_flex">
+            <div className="logo width">
+              <img src={logo} alt="" />
+            </div>
+            <div className="search-box f_flex">
+              <i className="fa fa-search"></i>
+              <input type="text" placeholder='Search and hit enter...' />
+              <span>All Category</span>
+            </div>
+            <div className="icon f_flex width">
+              <i className="fa fa-user icon_circle"></i>
+              <div className="cart">
+                <Link to='/cart'>
+                  <i className="fa fa-shopping-bag icon"></i>
+                  <span>0</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
     </div>
   )
 }
